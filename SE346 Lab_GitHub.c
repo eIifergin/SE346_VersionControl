1da5f875955fa0cf0364b67787fa806f6fb71465
eIifergin <ergin.elif@student.atilim.edu.tr>
2022-05-13 23:15:50 +0300

    v1.2

--- SE346 Lab_GitHub.c
+++ SE346 Lab_GitHub.c
@@ -1,13 +1,6 @@
 #include<stdio.h>
 
-int main()
-{
-	printf("SE346 Version Control:\n");
+int main (){
 	
-	//please write your name,student id and department by using printf() function
-
-	printf("Z.Yaren Oguz,123456,SE\n");
-
-	printf("Ozge Tekin,987654,SE\n");
-
+	printf("Elif Ergin 19243610024 ISE");
 }
